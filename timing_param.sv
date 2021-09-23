0ed4b034ebd02dff8b91a7ba6ebcdcf7ac53db62
Michael Singer <michael@a-singer.de>
2021-09-23 22:51:54 +0200

    Add timing parameters for missing cells

--- timing_param.sv
+++ timing_param.sv
@@ -1,20 +1,21 @@
 `default_nettype none
 
 parameter time T_INV     = 2ns;
-parameter time T_AND     = 4ns;
 parameter time T_NAND    = 2ns;
-parameter time T_OR      = 4ns;
 parameter time T_NOR     = 2ns;
-parameter time T_OA      = 6ns;
-parameter time T_AO      = 6ns;
-parameter time T_NAO     = 5ns;
-parameter time T_MUX     = 6ns;
-parameter time T_MUXI    = 6ns;
-parameter time T_AOI     = 5ns;
-parameter time T_XOR     = 5ns;
+parameter time T_AND     = 4ns;
+parameter time T_OR      = 4ns;
 parameter time T_XNOR    = 5ns;
+parameter time T_XOR     = 5ns;
+parameter time T_AOI     = 5ns;
+parameter time T_AO      = 6ns;
+parameter time T_OA      = 6ns;
+parameter time T_MUXI    = 5ns;
+parameter time T_MUX     = 6ns;
+parameter time T_HADD    = 6ns;
 parameter time T_ADD     = 6ns;
 parameter time T_TRI     = 2ns;
+parameter time T_TRIB    = 4ns;
 parameter time T_SRL     = 4ns;
 parameter time T_DL      = 4ns;
 parameter time T_DFF     = 8ns;
