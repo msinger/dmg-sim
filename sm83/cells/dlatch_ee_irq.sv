56f2958b8662d74dca37b922bd822bb5e60cb4a5
Michael Singer <michael@a-singer.de>
2025-11-15 05:44:36 +0100

    Fix interrupt priority logic

--- sm83/cells/dlatch_ee_irq.sv
+++ sm83/cells/dlatch_ee_irq.sv
@@ -5,8 +5,9 @@ module sm83_dlatch_ee_irq #(
 		parameter real L_q_n       = 25,
 		parameter real L_gated_q_n = 37
 	) (
-		input  logic d, ena, ena_n, pch_n, ena_q_n,
-		output logic q, q_n, gated_q_n
+		input      logic d, ena, ena_n, pch_n,
+		inout  tri logic ena_q_n, // Needs to propagate strength, therefore inout
+		output     logic q, q_n, gated_q_n
 	);
 
 	import sm83_timing::*;
@@ -19,7 +20,7 @@ module sm83_dlatch_ee_irq #(
 	assign q = !q_n;
 
 	bufif0 (strong1, highz0) (gated_q, '1, pch_n);
-	bufif1 (highz1, strong0) (gated_q, '0, q_n & !ena_q_n);
+	rnmos (gated_q, ena_q_n, q_n & (ena_q_n !== 1));
 	keeper gated_q_keeper(gated_q);
 	assign gated_q_n = !gated_q;
 
