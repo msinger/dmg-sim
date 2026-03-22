e5baa8d3aefda1c4e3aee0fb922a4eab42d0ce34
Michael Singer <michael@a-singer.de>
2026-03-22 21:49:14 +0100

    Let test cases compare cartridge RAM dumps

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -8,6 +8,9 @@ ROOT_DIR=$TESTS_DIR/..
 PNG_PATH=$TESTS_DIR/data/$1/result.png
 PNG_ALT_PATH=$TESTS_DIR/data/$1/result-alt.png
 PNG_XFAIL_PATH=$TESTS_DIR/data/$1/result-xfail.png
+SAV_PATH=$TESTS_DIR/data/$1/result.sav
+SAV_ALT_PATH=$TESTS_DIR/data/$1/result-alt.sav
+SAV_XFAIL_PATH=$TESTS_DIR/data/$1/result-xfail.sav
 SETUP_PATH=$TESTS_DIR/data/$1/setup.sh
 ROM_PATH=$TESTS_DIR/roms/$1.gb
 
@@ -38,6 +41,7 @@ tmpdir=$(mktemp -d)
 vvp -N "$ROOT_DIR"/dmg_cpu_b_gameboy.vvp \
 	-none \
 	+VID_FILE="$tmpdir"/test.vid \
+	+SAV_FILE="$tmpdir"/test.sav \
 	+BOOTROM="$ROOT_DIR"/boot/quickboot.bin \
 	+ROM="$ROM_PATH" \
 	+SECS=$SECS
@@ -49,23 +53,53 @@ cd -- "$tmpdir"
 if [ -d "$TESTS_DIR/logs/$1" ]; then
 	magick -size 160x144 -depth 8 RGB:$(printf img%06d.rgb $FRAME) out.png
 	mv out.png "$TESTS_DIR/logs/$1/"
+	if [ -r test.sav ]; then
+		ln test.sav "$TESTS_DIR/logs/$1/out.sav"
+	fi
 fi
+
+PASSED=
+XFAILED=
 
 if [ -r "$PNG_PATH" ]; then
 	magick "$PNG_PATH" RGB:result.rgb
 	if cmp $(printf img%06d.rgb $FRAME) result.rgb; then
-		exit 0
-	if [ -r "$PNG_ALT_PATH" ]; then
+		PASSED=y
+	elif [ -r "$PNG_ALT_PATH" ]; then
 		magick "$PNG_ALT_PATH" RGB:result.rgb
 		if cmp $(printf img%06d.rgb $FRAME) result.rgb; then
-			exit 0
+			PASSED=y
 		fi
 	fi
 fi
 if [ -r "$PNG_XFAIL_PATH" ]; then
 	magick "$PNG_XFAIL_PATH" RGB:result.rgb
 	if cmp $(printf img%06d.rgb $FRAME) result.rgb; then
-		exit 32
+		XFAILED=y
 	fi
 fi
+
+if [ -r "$SAV_PATH" ]; then
+	if cmp test.sav "$SAV_PATH"; then
+		PASSED=y
+	elif [ -r "$SAV_ALT_PATH" ]; then
+		if cmp test.sav "$SAV_ALT_PATH"; then
+			PASSED=y
+		fi
+	fi
+fi
+if [ -r "$SAV_XFAIL_PATH" ]; then
+	if cmp test.sav "$SAV_XFAIL_PATH"; then
+		XFAILED=y
+	fi
+fi
+
+if [ -n "$XFAILED" ]; then
+	exit 32
+fi
+
+if [ -n "$PASSED" ]; then
+	exit 0
+fi
+
 exit 1
